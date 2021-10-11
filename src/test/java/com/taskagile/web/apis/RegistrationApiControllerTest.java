80db855a3eca571c1084bf69366e6df8ebb0a2cf
babjo <kd980311@naver.com>
2021-10-11 14:39:53 +0900

    Add spring security

--- src/test/java/com/taskagile/web/apis/RegistrationApiControllerTest.java
+++ src/test/java/com/taskagile/web/apis/RegistrationApiControllerTest.java
@@ -12,9 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.taskagile.config.SecurityConfiguration;
 import com.taskagile.domain.application.UserService;
 import com.taskagile.domain.model.user.EmailAddressExistsException;
 import com.taskagile.domain.model.user.UsernameExistsException;
@@ -22,7 +25,9 @@ import com.taskagile.utils.JsonUtils;
 import com.taskagile.web.payload.RegistrationPayload;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(RegistrationApiController.class)
+@ContextConfiguration(classes = { SecurityConfiguration.class, RegistrationApiController.class })
+@WebMvcTest
+@ActiveProfiles("test")
 public class RegistrationApiControllerTest {
 
     @Autowired
